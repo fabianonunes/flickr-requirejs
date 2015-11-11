59a65680eca52d796db01b50781f90887e86424d
fabianonunes <fabiano@nunes.me>
2015-11-10 23:39:41 -0200

    reescrever módulo flickr e permitir parametrização do user_id

--- flickrApi.js
+++ flickrApi.js
@@ -1,11 +1,11 @@
 define(['q', 'jsonp'], function (q, jsonp) {
 
   var dfd = q.defer(),
-      url = '//api.flickr.com/services/feeds/photos_friends.gne'
+      url = '//api.flickr.com/services/feeds/photos_public.gne'
 
   jsonp({
     url: url,
-    data: { format: 'json', user_id: '49143546@N06' },
+    data: { format: 'json', id: '49143546@N06' },
     callbackName: 'jsoncallback',
     success: dfd.resolve.bind(dfd),
     error: dfd.reject.bind(dfd)
