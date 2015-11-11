a6586c75210e1b27f62060046552512eba729c4c
fabianonunes <fabiano@nunes.me>
2015-11-11 01:39:49 -0200

    estilos

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ require(['jquery', './flickrApi'], function ($, flickr) {
   flickr
     .photosPublic('49143546@N06')
     .then(function (data) {
+      return data.items.sort(function () {
+        return Math.random() > Math.random()
+      })
+    })
+    .then(function (items) {
 
-      var html = data.items.map(function (item) {
+      var html = items.map(function (item) {
         return '<img src=' + item.media.m + '>'
       }).join('')
 
